c087d752aca04e9ad382f24f7e9c04d6b7af1262
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2023-01-14 20:57:38 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yubowen/ybw/gtest/src/googletest-release-1.10.0
-# Build directory: /home/yubowen/ybw/gtest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yubowen/ybw/gtest/src/googletest-release-1.10.0/googletest
-# Build directory: /home/yubowen/ybw/gtest/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
