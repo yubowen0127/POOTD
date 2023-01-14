371aba00aaaa0449da516ed8a53491d2d06e7b87
yubowen0127 <114236074+yubowen0127@users.noreply.github.com>
2023-01-14 20:56:25 +0100

    Add files via upload

---
+++ build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/yubowen/ybw/gtest/src/googletest-release-1.10.0/googlemock
+# Build directory: /home/yubowen/ybw/gtest/build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yubowen/ybw/gtest/src/googletest-release-1.10.0/googletest
+# Build directory: /home/yubowen/ybw/gtest/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
